32794a4e05e137e4c1f96053b8ee66ee4e292793
Tanya <121035554+tanya232003@users.noreply.github.com>
2023-04-18 22:36:30 +0530

    Add files via upload

---
+++ conc_two_string.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class conc_two_string {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextLine()){
+        String word_1 = sc.next();
+        String word_2 = sc.next();
+
+        System.out.println(word_1+word_2);
+    }
+
+
+}
+}
